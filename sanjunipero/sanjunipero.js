022beea08f19d9d09da1f5b25003b056959a1482
Jordan-Tyler <kess555@usc.edu>
2020-04-09 17:56:27 -0700

    new without poetry

--- sanjunipero/sanjunipero.js
+++ sanjunipero/sanjunipero.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
 
+  var i = 1;
+
+  var images = new Array
+  images[0] = "title.jpg"
+  images[1] = "tuckers.jpg";
+  images[2] = "yorkiealone.jpg";
+  images[3] = "kellyandyorkiemeet.jpg";
+  images[4]= "dance.jpg"
+
+
+  $("#rightarrow").click(function(){
+    $("#img").attr("src", images[i]);
+    i++;
+  });
+
+  $("#leftarrow").click(function(){
+    $("#img").attr("src", images[i]);
+    i--;
+  });
+
 
 });
