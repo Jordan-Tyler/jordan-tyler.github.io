3aa4f32e898c2ef40802036859a3b5dac8e28027
Jordan-Tyler <kess555@usc.edu>
2020-04-09 18:00:38 -0700

    K

--- sanjunipero/sanjunipero.js
+++ sanjunipero/sanjunipero.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   images[0] = "title.jpg"
   images[1] = "tuckers.jpg";
   images[2] = "yorkiealone.jpg";
-  images[3] = "kellyandyorkiemeet.jpg";
+  images[3] = "Kellyandyorkiemeet.jpg";
   images[4]= "dance.jpg"
 
 
